77d05d3fcf986be982c61e589e5cd139a0b0cbd5
jayesh8913 <jayeshpatil@030405@gmail.com>
2025-12-05 13:15:44 +0530

    boilerplate

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());    
+
+app.post("/add-liquidity",(req,res)=>{
+
+})
+
+app.post("/buy-asset",(req,res)=>{
+
+})
+
+app.post("/sell-asset",(req,res)=>{
+
+})
+
+app.post("/quote",(req,res)=>{
+
+})
+
+app.listen(3000);
