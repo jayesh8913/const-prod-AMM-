00cc4c4d4145b7cc3a72fb0c2ffb7b11d3f76908
jayesh patil <jayeshpatil030405@gmail.com>
2025-12-06 00:09:01 +0530

    add final code

--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 trying to make a simple automatic market maker(AMM) using constant product algorithm with a bit of express 
+
+additional info about how it works under the hood :- https://whiteboardcrypto.com/what-is-an-automated-market-maker/
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,44 @@
 import express from "express";
-
+import { Request, Response } from "express";
 const app = express();
 app.use(express.json());    
 
-app.post("/add-liquidity",(req,res)=>{
+let usdc_quant = 100000;
+let sol_quant= 500;
+/*app.post("/add-liquidity",(req,res)=>{
+
+})*/
+
+app.post("/buy-asset",(req: Request, res: Response)=>{
+    const quantity = Number(req.body.quantity);
+    let updatedsol = sol_quant- quantity;
+    let updatedusdc = sol_quant*usdc_quant/updatedsol;    // as the const remains same X*Y=updatedX*updatedY
+    const paidamount = updatedusdc -usdc_quant;
+
+    usdc_quant=updatedusdc; 
+    sol_quant=updatedsol;
+    res.json({
+        message:  `you paid ${paidamount} usdc for ${quantity} of sol`
+    })
+    
+})
+
+app.post("/sell-asset",(req: Request, res: Response)=>{
+    const quantity=Number(req.body.quantity);
+    let updatedsol = sol_quant + quantity;
+    let updatedusdc = sol_quant*usdc_quant/updatedsol   ;
+    const gottenusdc = usdc_quant- updatedusdc;
+        
+    usdc_quant=updatedusdc;
+    sol_quant=updatedsol;
+    res.json({
+        message:  `you got ${gottenusdc} usdc for ${quantity} of sol `
+    })
+    
 
 })
 
-app.post("/buy-asset",(req,res)=>{
-
-})
-
-app.post("/sell-asset",(req,res)=>{
-
-})
-
-app.post("/quote",(req,res)=>{
+app.post("/quote",(req: Request, res: Response)=>{
 
 })
 
